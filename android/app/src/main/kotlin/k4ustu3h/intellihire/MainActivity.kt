705137a827c976aa28c0481fe78f6a2ca40e1b3e
Kaustubh Ladiya <k4ustu3h@gmail.com>
2025-07-27 16:21:52 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/k4ustu3h/intellihire/MainActivity.kt
@@ -0,0 +1,5 @@
+package k4ustu3h.intellihire
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:intellihire/pages/homepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'IntelliHire',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomePage(title: 'IntelliHire'),
+    );
+  }
+}
