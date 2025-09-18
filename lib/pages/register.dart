979df23cf05b72106c2beda6121120ceaa067a35
Kaustubh Ladiya <k4ustu3h@gmail.com>
2025-09-18 15:29:21 +0530

    Redesign Register page
    Added
      - simple_icons
      - Name field in Register page

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,5 +1,8 @@
 import "package:firebase_auth/firebase_auth.dart";
 import "package:flutter/material.dart";
+import "package:intellihire/pages/login.dart";
+import "package:material_symbols_icons/symbols.dart";
+import "package:simple_icons/simple_icons.dart";
 
 class Register extends StatefulWidget {
   const Register({super.key});
@@ -9,15 +12,68 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  final _nameController = TextEditingController();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
 
+  late final List<Map<String, dynamic>> _fields;
+
+  @override
+  void initState() {
+    super.initState();
+    _fields = [
+      {
+        "controller": _nameController,
+        "labelText": "Name",
+        "iconName": "name",
+        "obscureText": false,
+      },
+      {
+        "controller": _emailController,
+        "labelText": "Email",
+        "iconName": "email",
+        "obscureText": false,
+      },
+      {
+        "controller": _passwordController,
+        "labelText": "Password",
+        "iconName": "password",
+        "obscureText": true,
+      },
+    ];
+  }
+
+  final List<Map<String, dynamic>> _buttons = [
+    {"label": "Continue with Google", "iconName": "google"},
+    {"label": "Continue with Apple", "iconName": "apple"},
+  ];
+
+  IconData _getIconData(String iconName) {
+    switch (iconName) {
+      case "apple":
+        return SimpleIcons.apple;
+      case "email":
+        return Symbols.email_rounded;
+      case "name":
+        return Symbols.person_rounded;
+      case "google":
+        return SimpleIcons.google;
+      case "password":
+        return Symbols.password;
+      default:
+        return Symbols.error;
+    }
+  }
+
   Future<void> signUp() async {
     try {
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: _emailController.text.trim(),
-        password: _passwordController.text.trim(),
-      );
+      final userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+            email: _emailController.text.trim(),
+            password: _passwordController.text.trim(),
+          );
+      await userCredential.user?.updateDisplayName(_nameController.text.trim());
+
       if (mounted) {
         Navigator.of(context).pop();
       }
@@ -40,6 +96,7 @@ class _RegisterState extends State<Register> {
 
   @override
   void dispose() {
+    _nameController.dispose();
     _emailController.dispose();
     _passwordController.dispose();
     super.dispose();
@@ -47,39 +104,71 @@ class _RegisterState extends State<Register> {
 
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
     return Scaffold(
-      appBar: AppBar(title: Text("Register")),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(MediaQuery.of(context).padding.top),
+        child: SizedBox(height: MediaQuery.of(context).padding.top),
+      ),
       body: Padding(
-        padding: EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(16),
         child: Column(
+          spacing: 16,
           children: [
-            Padding(
-              padding: EdgeInsets.only(bottom: 16),
-              child: TextField(
-                controller: _emailController,
+            Text("Create an Account", style: textTheme.displaySmall),
+            Text("Enter your details", style: textTheme.titleMedium),
+            ..._fields.map((field) {
+              return TextField(
+                controller: field["controller"],
                 decoration: InputDecoration(
+                  prefixIcon: Icon(_getIconData(field["iconName"])),
                   border: OutlineInputBorder(),
-                  labelText: "Email",
+                  labelText: field["labelText"],
                 ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 16),
-              child: TextField(
-                controller: _passwordController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Password",
-                ),
-                obscureText: true,
-              ),
-            ),
+                obscureText: field["obscureText"],
+              );
+            }),
             FilledButton(
               onPressed: signUp,
               style: FilledButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
                 minimumSize: Size(double.infinity, 48),
               ),
               child: Text("Sign Up"),
+            ),
+            Row(
+              children: [
+                Expanded(child: Divider()),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Text("Or", style: TextStyle(color: Colors.grey)),
+                ),
+                Expanded(child: Divider()),
+              ],
+            ),
+            ..._buttons.map((button) {
+              return FilledButton.icon(
+                onPressed: () {},
+                icon: Icon(_getIconData(button["iconName"])),
+                style: FilledButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  minimumSize: Size(double.infinity, 48),
+                ),
+                label: Text(button["label"]),
+              );
+            }),
+            TextButton(
+              onPressed: () {
+                Navigator.of(
+                  context,
+                ).push(MaterialPageRoute(builder: (context) => Login()));
+              },
+              child: Text("Already have an account? Login here."),
             ),
           ],
         ),
