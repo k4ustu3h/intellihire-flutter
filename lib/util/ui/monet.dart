2522118b76535d971626e9d0886bca7d56ca0da6
Kaustubh Ladiya <k4ustu3h@gmail.com>
2025-10-14 19:16:08 +0530

    Add shape morphs to buttons and thumb icons to switches

--- lib/util/ui/monet.dart
+++ lib/util/ui/monet.dart
@@ -1,5 +1,6 @@
 import "package:dynamic_color/dynamic_color.dart";
 import "package:flutter/material.dart";
+import "package:material_symbols_icons/material_symbols_icons.dart";
 
 const _brandSeedColor = Colors.blueAccent;
 
@@ -12,9 +13,37 @@ final _darkColorScheme = ColorScheme.fromSeed(
 ThemeData _buildTheme(ColorScheme baseScheme) {
   final scheme = baseScheme.harmonized();
 
+  final buttonShape = WidgetStateProperty.resolveWith<OutlinedBorder>((states) {
+    final radius = states.contains(WidgetState.pressed) ? 8.0 : 24.0;
+    return RoundedRectangleBorder(borderRadius: BorderRadius.circular(radius));
+  });
+
+  final switchTheme = SwitchThemeData(
+    thumbIcon: WidgetStateProperty.resolveWith<Icon?>((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Icon(Symbols.check_rounded, size: 18);
+      } else {
+        return Icon(Symbols.close_rounded, size: 18);
+      }
+    }),
+  );
+
   return ThemeData(
     colorScheme: scheme,
     bottomSheetTheme: BottomSheetThemeData(showDragHandle: true),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(shape: buttonShape),
+    ),
+    filledButtonTheme: FilledButtonThemeData(
+      style: ButtonStyle(shape: buttonShape),
+    ),
+    outlinedButtonTheme: OutlinedButtonThemeData(
+      style: ButtonStyle(shape: buttonShape),
+    ),
+    switchTheme: switchTheme,
+    textButtonTheme: TextButtonThemeData(
+      style: ButtonStyle(shape: buttonShape),
+    ),
   );
 }
 
